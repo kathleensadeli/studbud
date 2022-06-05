edd9b20bb390647abfae8060a6009d6c6388e0cd
kathleensadeli <100390897+kathleensadeli@users.noreply.github.com>
2022-06-05 17:58:50 +0700

    Change music to Lofi

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const dotenv = require('dotenv');
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const musics = './static/musics/';
-const fs = require('fs');
-const jsmediatags = require("jsmediatags");
 
 
 const app = express();
@@ -32,94 +30,94 @@ mongoose.connect(process.env.DB_CONNECT).then(()=>{
 
 const trackList=[{
     id:1,
-    name:"Indigo",
-    artist:"Niki",
-    image:"static/musics/images/niki1.png",
-    path:"static/musics/indigo.mp3"
+    name:"Bedtime after a coffee",
+    artist:"Barradeen",
+    image:"static/musics/images/Barradeen.jpg",
+    path:"static/musics/bedtime.mp3"
 },{
     id:2,
-    name:"Shivers",
-    artist:"Ed Sheeran",
-    image:"static/musics/images/edsheeran1.png",
-    path:"static/musics/shivers.mp3"
+    name:"Devyzed Downtown",
+    artist:"Ghostrifter Official",
+    image:"static/musics/images/Ghostrifter official.jpg",
+    path:"static/musics/devyzed.mp3"
 },{
     id:3,
-    name:"Leave Your Life",
-    artist:"Ed Sheeran",
-    image:"static/musics/images/edsheeran1.png",
-    path:"static/musics/leaveyourlife.mp3"
+    name:"Ethereal",
+    artist:"Ghostrifter Official",
+    image:"static/musics/images/Ghostrifter official.jpg",
+    path:"static/musics/ethereal.mp3"
 },{
     id:4,
-    name:"Stop The Rain",
-    artist:"Ed Sheeran",
-    image:"static/musics/images/edsheeran1.png",
-    path:"static/musics/stoptherain.mp3"
+    name:"Morning Routine",
+    artist:"Ghostrifter Official",
+    image:"static/musics/images/Ghostrifter official.jpg",
+    path:"static/musics/morningroutine.mp3"
 },{
     id:5,
-    name:"Perfect",
-    artist:"Ed Sheeran",
-    image:"static/musics/images/edsheeran2.png",
-    path:"static/musics/perfect.mp3"
+    name:"Flamingo",
+    artist:"Kidcut",
+    image:"static/musics/images/kidcut.jpg",
+    path:"static/musics/flamingo.mp3"
 },{
     id:6,
-    name:"Bad Habits",
-    artist:"Ed Sheeran",
-    image:"static/musics/images/edsheeran1.png",
-    path:"static/musics/badhabits.mp3"
+    name:"End Credits",
+    artist:"Mountaineer",
+    image:"static/musics/images/Mountaineer.jpg",
+    path:"static/musics/endcredits.mp3"
 },{
     id:7,
-    name:"Willow",
-    artist:"Taylor Swift",
-    image:"static/musics/images/taylor1.png",
-    path:"static/musics/willow.mp3"
+    name:"Fly Away",
+    artist:"Mountaineer",
+    image:"static/musics/images/Mountaineer.jpg",
+    path:"static/musics/flyaway.mp3"
 },{
     id:8,
-    name:"Positions",
-    artist:"Arianna Grande",
-    image:"static/musics/images/arianna1.png",
-    path:"static/musics/positions.mp3"
+    name:"Cozy",
+    artist:"Prigida",
+    image:"static/musics/images/Prigida.jpg",
+    path:"static/musics/cozy.mp3"
 },{
     id:9,
-    name:"Closure",
-    artist:"Taylor Swift",
-    image:"static/musics/images/taylor1.png",
-    path:"static/musics/closure.mp3"
+    name:"Mellow Bop",
+    artist:"Prigida",
+    image:"static/musics/images/Prigida.jpg",
+    path:"static/musics/mellowbop.mp3"
 },{
     id:10,
-    name:"Visiting Hours",
-    artist:"Ed Sheeran",
-    image:"static/musics/images/edsheeran1.png",
-    path:"static/musics/visitinghours.mp3"
+    name:"Moonshine",
+    artist:"Prigida",
+    image:"static/musics/images/Prigida.jpg",
+    path:"static/musics/moonshine.mp3"
 },{
     id:11,
-    name:"Shape of You",
-    artist:"Ed Sheeran",
-    image:"static/musics/images/edsheeran2.png",
-    path:"static/musics/shapeofyou.mp3"
+    name:"Midnight Snack",
+    artist:"Purrple Cat",
+    image:"static/musics/images/Purrple cat.jpg",
+    path:"static/musics/midnightsnack.mp3"
 },{
     id:12,
-    name:"Gold Rush",
-    artist:"Taylor Swift",
-    image:"static/musics/images/taylor1.png",
-    path:"static/musics/goldrush.mp3"
+    name:"Missing You",
+    artist:"Purrple Cat",
+    image:"static/musics/images/Purrple cat.jpg",
+    path:"static/musics/missingyou.mp3"
 },{
     id:13,
-    name:"Test Drive",
-    artist:"Arianna Grande",
-    image:"static/musics/images/arianna1.png",
-    path:"static/musics/testdrive.mp3"
+    name:"Your Love is What I Need",
+    artist:"Soundroll",
+    image:"static/musics/images/Soundroll.jpg",
+    path:"static/musics/yourloveiswhatineed.mp3"
 },{
     id:14,
-    name:"Majorie",
-    artist:"Taylor Swift",
-    image:"static/musics/images/taylor1.png",
-    path:"static/musics/majorie.mp3"
+    name:"Mellow Skies",
+    artist:"Tatami",
+    image:"static/musics/images/Tatami.jpg",
+    path:"static/musics/mellowskies.mp3"
 },{
     id:15,
-    name:"Galway Girl",
-    artist:"Ed Sheeran",
-    image:"static/musics/images/edsheeran2.png",
-    path:"static/musics/galwaygirl.mp3"
+    name:"Name",
+    artist:"Walz",
+    image:"static/musics/images/Walz.jpg",
+    path:"static/musics/name.mp3"
 }];
 
 
@@ -290,54 +288,3 @@ app.get("/playlist", (req, res) => {
 
 
 
-
-function loadPlaylist(callback){
-    fs.readdir(musics, (err, files) => {
-            if(err){
-                console.log(err);
-            }else{
-                // create array of getTags() promises
-                let promises = files.map(file => getTags(file))
-                Promise.all(promises).then(results=>{
-                    callback(null,results);
-                }).catch(err=> console.log('One of the requests failed'))
-            }
-            
-    });
-}
-
-function getTags(file) {      
-    return new Promise((resolve, reject) => {
-        jsmediatags.read("static/musics/"+file, {
-            onSuccess: (tag) => {   
-                let music = {
-                    'title':null,
-                    'artist':null,
-                    'path':null,
-                    'image':null,
-                    'format':null,
-                };
-                let image = tag.tags.picture;
-                music['title']=tag.tags.title;
-                music['artist']=tag.tags.artist;
-                music['path']='/static/musics/'+file;
-
-                if (image) {
-                    music['format']=image.format;
-                    var base64String = "";
-                    for (var i = 0; i < image.data.length; i++) {
-                        base64String += String.fromCharCode(image.data[i]);
-                    }
-                    // var base64 = "data:" + image.format + ";base64," +window.btoa(base64String);
-                    // music['image']=base64String;
-                    // document.getElementById('picture').setAttribute('src',base64);
-                    
-                }            
-                resolve( music);
-            },
-            onError: (error) => {                
-            reject(error);
-            }
-        });
-    });
-}
